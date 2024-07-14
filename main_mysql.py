c46556c852f3245b77b918ef23f43bd94a8b6c16
Rahil-B <rahilsrb@gmail.com>
2024-07-15 00:56:05 +0530

    Restructured files

--- main_mysql.py
+++ main_mysql.py
@@ -5,29 +5,29 @@ from database import create_connection, create_student, get_students, get_studen
 app = FastAPI()
 connection = create_connection()
 
-@app.post("/students/")
+@app.post("/sql/students/")
 def api_create_student(student: Student):
     create_student(connection, student.name, student.age, student.grade)
     return {"message": "Student created successfully"}
 
-@app.get("/students/")
+@app.get("/sql/students/")
 def api_get_students():
     students = get_students(connection)
     return students
 
-@app.get("/students/{student_id}")
+@app.get("/sql/students/{student_id}")
 def api_get_student(student_id: int):
     student = get_student_by_id(connection, student_id)
     if not student:
         raise HTTPException(status_code=404, detail="Student not found")
     return student
 
-@app.put("/students/{student_id}")
+@app.put("/sql/students/{student_id}")
 def api_update_student(student_id: int, student: Student):
     update_student(connection, student_id, student.name, student.age, student.grade)
     return {"message": "Student updated successfully"}
 
-@app.delete("/students/{student_id}")
+@app.delete("/sql/students/{student_id}")
 def api_delete_student(student_id: int):
     delete_student(connection, student_id)
     return {"message": "Student deleted successfully"}
--- readme.md
+++ readme.md
@@ -1,3 +1,36 @@
-uvicorn main:app --reload  
+# MySQL
+docker run --name mysql-container -e MYSQL_ROOT_PASSWORD=root -p 3306:3306 -d mysql:latest
+
+CRETE DATABASE school;
+
+CREATE TABLE students (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ name VARCHAR(100) NOT NULL,
+ age INT NOT NULL,
+ grade VARCHAR(10) NOT NULL
+);
+
+pip install fastapi uvicorn
+pip3 install mysql-connector-python           
+
+#to run the Fast API
+uvicorn main_mysql:app --reload  
+
+# docker exec -it mysql-container bash -- to run the container terminal
+# MongoDB
+pip3 install pymongo
+docker pull mongo:latest
+docker run -d -p 27017:27017 --name=mongodb-container mongo:latest
+
 
 uvicorn main_mongo:app --reload  
+
+# Elastic Search
+uvicorn main_elastic:app --reload  
+
+docker network create elastic
+docker pull docker.elastic.co/elasticsearch/elasticsearch:8.14.3
+docker run --name es01 --net elastic -p 9200:9200 -it -m 1GB docker.elastic.co/elasticsearch/elasticsearch:8.14.3
+
+
+# Repo URL : https://github.com/Rushi-Varmora05/Assignment-5
