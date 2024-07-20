abd83a3bd4e46b83e575766cd9a6691d1b380810
Rahil-B <rahilsrb@gmail.com>
2024-07-20 23:46:52 +0530

    Finalized neo4j

--- database_neo4j.py
+++ database_neo4j.py
@@ -42,6 +42,8 @@ class MyNeo4jDB:
 
     def serialize(self, data):
         serialized_data = []
+        if data[0] is None:
+            return []
         for student in data:
             serialized_student = {
                 "_id": student.id,
